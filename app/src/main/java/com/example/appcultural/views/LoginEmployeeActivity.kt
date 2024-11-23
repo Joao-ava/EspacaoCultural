84a92c1030df9e444b05b5dd15904d1b748c8667
Joao-ava <ajoao781238@gmail.com>
2024-11-22 23:23:34 -0300

    Verificar se é admin

--- app/src/main/java/com/example/appcultural/views/LoginEmployeeActivity.kt
+++ app/src/main/java/com/example/appcultural/views/LoginEmployeeActivity.kt
@@ -3,6 +3,7 @@ package com.example.appcultural.views
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -19,25 +20,36 @@ class LoginEmployeeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginEmployeeBinding.inflate(layoutInflater)
         setContentView(binding.main)
-        val authProvider = FirebaseAuthProvider()
 
-        val intent = Intent(this, BottomActivity::class.java)
         binding.loginEmployeeButton.setOnClickListener {
-            lifecycleScope.launch {
-                try {
-                    authProvider.login(binding.editTextTextEmail.editText?.text.toString(), binding.loginEmployeePassword.editText?.text.toString())
-                    authProvider.getAdmin(context = this@LoginEmployeeActivity)
-                    startActivity(intent)
-                } catch (e: Exception) {
-                    Toast.makeText(baseContext, "Autenticação falhou: " + e.message,
-                        Toast.LENGTH_SHORT).show()
-                }
-            }
+            login()
         }
+        binding.loginEmployeePassword.editText?.setOnEditorActionListener { v, actionId, event ->  run {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                login()
+            }
+            false
+        }}
 
         binding.signupButton.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun login() {
+        val intent = Intent(this, BottomActivity::class.java)
+        lifecycleScope.launch {
+            try {
+                val authProvider = FirebaseAuthProvider()
+                authProvider.login(binding.editTextTextEmail.editText?.text.toString(), binding.loginEmployeePassword.editText?.text.toString())
+                val isAdmin = authProvider.getAdmin(context = this@LoginEmployeeActivity)
+                if (!isAdmin) throw Exception("Usuário não é administrador")
+                startActivity(intent)
+            } catch (e: Exception) {
+                Toast.makeText(baseContext, "Autenticação falhou: " + e.message,
+                    Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
